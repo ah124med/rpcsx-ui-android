2e9042ae93f86b56c6fd0e9a3fedeb6a14244e62
ah124med <ah689med@gmail.com>
2025-05-06 10:08:32 +0400

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,7 @@
 #include <android/log.h>
 #include <thread>
 #include "rpcsx/rpcsx.h" // Assume RPCSX API exists
+#include <vulkan/vulkan.h>
 
 #define LOG_TAG "NativeRPCSX"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
